129dee8865a202fe1080e01000acdbb20f6f218e
mamiotani <mamia_otani@docomo.ne.jp>
2020-04-13 22:06:01 +0900

    課題完成しました。

--- app/src/main/java/jp/techacademy/mami/ootani/Human.kt
+++ app/src/main/java/jp/techacademy/mami/ootani/Human.kt
@@ -3,23 +3,17 @@ package jp.techacademy.mami.ootani
 
 import android.util.Log
 
-class Human: Animal {
-
-    constructor(name: String, age: Int) : super(name, age) {
-
-        fun say() {
-            Log.d("kotlintest", "私の名前は" + "(" + this.name + "です。)" + "歳は" + "(" + this.age + "歳です。)")
-        }
-    }
-}
-
-class Human: Thinkable {
+class Human: Animal, Thinkable {
     var hobby :String
 
-    constructor(hobby: String) {
-        this.hobby= hobby
+    constructor(name: String, age: Int, hobby: String) : super(name, age) {
+        this.hobby = hobby
+    }
+
+    override fun say() {
+        Log.d("kotlintest", "私の名前は"  + this.name + "です。" + "歳は"  + this.age + "歳です。")
     }
     override fun think() {
-        Log.d("kotlintest", "私は" + "("+ this.hobby + "について考える。)")
+        Log.d("kotlintest", "私は"  + this.hobby + "について考える。")
     }
 }
